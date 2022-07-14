b19a16ef3775c154286391ebbb9cc354f683eb82
Mango Jelly Pudding <evan940415@gmail.com>
2022-07-14 08:39:28 +0800

    fix: input.txt

--- 全國技藝競賽/2022.07.11/Q113：Power of Cryptography/main.py
+++ 全國技藝競賽/2022.07.11/Q113：Power of Cryptography/main.py
@@ -1,2 +1,8 @@
+from math import ceil, log
+
 with open('./input.txt') as file:
-    ...
+    lines = [int(i.removesuffix('\n')) for i in file.readlines()]
+    nums = [lines[2*i:2*i+2] for i in range(int(len(lines)/2))]
+
+for n, p in nums:
+    print(log(p, n))
