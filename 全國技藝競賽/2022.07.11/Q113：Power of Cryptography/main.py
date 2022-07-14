5a776ebd1eeac5011973585ba2327ea7aec3a2d1
Mango Jelly Pudding <evan940415@gmail.com>
2022-07-14 12:39:11 +0800

    finish 2022.07.11 Q113

--- 全國技藝競賽/2022.07.11/Q113：Power of Cryptography/main.py
+++ 全國技藝競賽/2022.07.11/Q113：Power of Cryptography/main.py
@@ -1,8 +1,6 @@
-from math import ceil, log
-
 with open('./input.txt') as file:
     lines = [int(i.removesuffix('\n')) for i in file.readlines()]
     nums = [lines[2*i:2*i+2] for i in range(int(len(lines)/2))]
 
-for n, p in nums:
-    print(log(p, n))
+with open('./output.txt', 'w') as file:
+    file.writelines('\n'.join(round(p**(1/n)) for n, p in nums))
