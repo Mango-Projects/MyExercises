3b3cda989b5e02a514e1226c883eb1166ae00e07
Mango Jelly Pudding <evan940415@gmail.com>
2022-08-15 14:54:10 +0800

    feat: pascal triangle finish half

--- 全國技藝競賽/2022.08.25/求係數/main.py
+++ 全國技藝競賽/2022.08.25/求係數/main.py
@@ -1,2 +1,13 @@
 with open('./input.txt') as file:
-    ...
+    numbers = map(int, file.readlines())
+
+def pascal_triangle(n):
+    L = [1]
+    new = []
+    for i in range(n):
+        new.append(L)
+        L = [sum(i) for i in zip([0]+L, L+[0])]
+    return new
+
+with open('./output.txt', 'w') as file:
+    file.write('\n'.join(', '.join(map(str, pascal_triangle(n+1)[n])) for n in numbers))
