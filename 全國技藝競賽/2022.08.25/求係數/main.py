fa6288874708fd7793caef246e97e72a4f000004
Mango Jelly Pudding <evan940415@gmail.com>
2022-08-25 13:17:05 +0800

    add: 求係數

--- 全國技藝競賽/2022.08.25/求係數/main.py
+++ 全國技藝競賽/2022.08.25/求係數/main.py
@@ -1,13 +1,13 @@
+from typing import Generator
+from math import factorial as f
+
+def binomial(n: int) -> Generator:
+    for k in range(n+1):
+        yield int(f(n)/(f(k)*f(n-k)))
+
 with open('./input.txt') as file:
     numbers = map(int, file.readlines())
 
-def pascal_triangle(n):
-    L = [1]
-    new = []
-    for i in range(n):
-        new.append(L)
-        L = [sum(i) for i in zip([0]+L, L+[0])]
-    return new
-
 with open('./output.txt', 'w') as file:
-    file.write('\n'.join(', '.join(map(str, pascal_triangle(n+1)[n])) for n in numbers))
+    for i in numbers:
+        file.write(', '.join(map(str, binomial(i)))+'\n')
