d48ce4a78bb0d8e56a85447775da215270556441
Mango Jelly Pudding <evan940415@gmail.com>
2022-08-25 13:17:22 +0800

    add: 聯立方程式

--- 全國技藝競賽/2022.08.25/聯立方程式/main.py
+++ 全國技藝競賽/2022.08.25/聯立方程式/main.py
@@ -1,25 +1,16 @@
+from typing import Iterable
+
+def equation(coef: Iterable[int]) -> tuple[int, int]:
+    a, b, c, d, e, f = coef
+    if (_:=a / d) != b / e:
+        return (-(c*e-b*f)/(a*e-b*d), -(c*d-a*f)/(b*d-a*e))
+    if _ == c/e:
+        return "inf"
+    return "no result"
+
 with open('./input.txt') as file:
-    coefficient = file.readline().split(',')
-    # a1, b1, c1, a2, b2, c2
+    coefficient = map(int, file.readline().split(','))
 
-def check_solution(coef):
-    a, b, c = coef[0]/coef[3], coef[1]/coef[4], coef[2]/coef[5]
-    if a != b: # a != b
-        return 1
-    if a == c: # a == b + a == c
-        return 0
-    return -1 # a == b + a != c
-
-"""
-1, 1, -2
-3, 1, -4
-ax + by + c = 0 -> y = -(a/b)x-c/b = -(1/1)x-(-2)/1 = -x+2
-dx + ey + f = 0 -> x = -f/(d+e(-(a/b)x-c/b)) = 4/(3+1(1/1)x-2) = 4/(4x-2) = 4
-
-
-
-"""
-
-for k, v in globals().copy().items():
-    if k[:2] != '__' and k[-2:] != '__':
-        print(f'{k}: {v}')
+with open('./output.txt', 'w') as file:
+    X, Y = map(int, equation(coef=coefficient))
+    file.write(f'{X=}, {Y=}')
