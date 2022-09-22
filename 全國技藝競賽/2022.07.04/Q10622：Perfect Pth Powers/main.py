14070bdf11e51c0f2a347a2961729b6461c36246
Mango Jelly Pudding <evan940415@gmail.com>
2022-09-22 15:13:45 +0800

    .w.

--- 全國技藝競賽/2022.07.04/Q10622：Perfect Pth Powers/main.py
+++ 全國技藝競賽/2022.07.04/Q10622：Perfect Pth Powers/main.py
@@ -1,2 +1,19 @@
-with open('./input.txt') as file:
-    ...
+"""
+x = b^p, max(b) = 1
+
+x          | p
+-----------|----
+17         | 1
+1073741824 | 30
+25         | 2
+-25        | 1
+64         | 6
+-64        | 3
+"""
+from sys import stdin
+from math import ceil
+
+for input_num in map(int, stdin.read().splitlines()):
+    powers = 1
+    for num in range(1, input_num+1):
+        ...
