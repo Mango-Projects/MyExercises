2b5c8ddf494e3331d123040cd38cdc28ef51be82
Mango Jelly Pudding <evan940415@gmail.com>
2022-10-19 14:38:08 +0800

    .w.

--- 全國技藝競賽/2022.10.18/八、即時工作排程.py
+++ 全國技藝競賽/2022.10.18/八、即時工作排程.py
@@ -1,28 +1,28 @@
 """
 2022.10.1 finish
 """
-# times = int(input())
-# data = []
-# for i in range(times):
-#     name, *value = input().split(' ')
-#     data.append((name, *map(int, value)))
-data = [('A1', 3, 3, 30, 8), ('A2', 6, 4, 25, 4), ('A3', 4, 5, 30, 4), ('A4', 8, 3, 30, 1)] # testdata
+times = int(input())
+data = []
+for i in range(times):
+    name, *value = input().split(' ')
+    data.append((name, *map(int, value)))
+# data = [('A1', 3, 3, 30, 8), ('A2', 6, 4, 25, 4), ('A3', 4, 5, 30, 4), ('A4', 8, 3, 30, 1)] # testdata
 
 waiting = sorted(data, key=lambda item: item[4])
 finish = []
 step = 1
 working = []
 while step:=step+1:
-    print(f'\n{"-"*50}\n{waiting}\n{finish}\n{step}\n{working}')
+    # print(f'\n{"-"*50}\n{waiting}\n{finish}\n{step}\n{working}')
     if not (waiting or working):
-        print('no waiting')
+        # print('no waiting')
         break
     if working:
-        print('has working')
-        if working[1]+working[2] < step:
-            print(f'working {working}')
+        # print('has working')
+        if step < working[1]+working[2]:
+            # print(f'working {working}')
             continue
-        print(f'finish {working}')
+        # print(f'finish {working}')
         finish.append(working)
         working = []
         continue
@@ -30,7 +30,7 @@ while step:=step+1:
         if step >= task[1]:
             working = task
             waiting.pop(index)
-            print(f'find task {working}, start:{working[1]}, runtime:{working[2]}, deadline:{working[3]}, priority:{working[4]}')
+            # print(f'find task {working}, start:{working[1]}, runtime:{working[2]}, deadline:{working[3]}, priority:{working[4]}')
             break
-print(f'\n{"-"*50}\n')
+# print(f'\n{"-"*50}\n')
 print(' '.join(item[0] for item in finish))
