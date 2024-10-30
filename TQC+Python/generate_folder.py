6d0371f2be475022dc19b333441f315c1ae26071
Mango Jelly Pudding <evan940415@gmail.com>
2024-10-30 16:18:52 +0800

    fix: update open mode

--- TQC+Python/generate_folder.py
+++ TQC+Python/generate_folder.py
@@ -1,10 +1,14 @@
-import os
+from pathlib import Path
+
+CWD = Path(__file__).parent
 
 tests = [104,110,202,204,306,310,404,406,504,510,602,610,704,706]
 
 for number in tests:
-	# os.mkdir(f"{number}/")
-	# with open(f"{number}/README.md", "w"):
-	# 	pass
-	with open(f"{number}/PYA{number}.py", "w"):
-		pass
+	try:
+		folder = CWD / f"{number}"
+		# folder.mkdir(parents=True,exist_ok=True)
+		# open(folder / "README.md", "a").close()
+		# open(folder / f"PYA{number}.py", "a").close()
+		open(folder / f"PYA{number}_pdf.py", "a").close()
+	except: pass
